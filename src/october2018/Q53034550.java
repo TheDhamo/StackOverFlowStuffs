5a156c5a1cf04c8fc0d44253da1d9880bfa6900f
Dhamo <thedhamo@gmail.com>
2018-11-01 22:16:04 -0600

    Adding today's answer

--- src/october2018/Q53034550.java
+++ src/october2018/Q53034550.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Q53034550 {
@@ -17,9 +19,18 @@ public class Q53034550 {
 
 	    Actions actions = new Actions(driver);
 	    WebElement ele = driver.findElement(By.xpath("//span[@class='nav-line-2' and contains(.,'Departments')]"));
+	    
+	    WebElement ele1 = driver.findElement((By.xpath("//span")));
 	    Thread.sleep(300);
+	    
+	    WebDriverWait wait=new WebDriverWait(driver, 10);
+	    
+	    WebElement guru99seleniumlink;
+		guru99seleniumlink= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//.")));
+	    
 	    actions.moveToElement(ele);
 	    actions.perform();
 	    actions.perform();
+
 	}
 }
