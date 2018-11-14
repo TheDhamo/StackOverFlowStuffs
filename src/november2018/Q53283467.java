6b4c6f6c88f6a59309045715717d1f3f47b68fd5
Dhamo <pkdhamotharan01@gmail.com>
2018-11-14 12:07:08 -0600

    Updated when author name not found. Learnings: To find, a element is present or not, we can use findElements<...>.size()>0?true:false;

--- src/november2018/Q53283467.java
+++ src/november2018/Q53283467.java
@@ -3,46 +3,47 @@ package november2018;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Q53283467 {
+	 
 	@Test
 	public static void testMF() {
 		WebDriver driver;
 		System.setProperty("webdriver.gecko.driver", "./src/drivers/geckodriver64bit.exe");
 		driver = new FirefoxDriver();
-		
+
 		driver.get(
 				"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=Java&rh=i%3Aaps%2Ck%3AJava");
-		WebElement searchResults = driver.findElement(By.cssSelector("#s-results-list-atf"));
 
-		List<WebElement> links = searchResults.findElements(By.tagName("li"));
-
-		if (links.size() > 0) {
-			String title, author, price, rating, isBestSeller;
-
-			for (int i = 0; i < links.size(); i++) {
-				title = links.get(i)
-						.findElement(By.cssSelector("h2.a-size-medium.s-inline.s-access-title.a-text-normal"))
-						.getText();
-				System.out.println("Title:"+title);
-				author = links.get(i).findElement(By.xpath("//span[@class='a-size-small a-color-secondary' and contains(.,'by')]/../span[2]")).getText();
-				System.out.println("Author:"+author);
-				
-				price = links.get(i).findElement(By.xpath("//span[@class='sx-price-whole']")).getText();
-				System.out.println("Price:"+price);
-				
-				rating= links.get(i).findElement(By.xpath("//span[@class='a-icon-alt']")).getText();
-				System.out.println("Rating:"+rating);
-				
-				
-				isBestSeller = links.get(i).findElement(By.xpath("//span[@class='a-badge-text' and contains(.,'Best Seller')]")).isDisplayed()?"Y":"N";
-				System.out.println("IsBestSeller:"+isBestSeller);
-			}
-		} else
-			System.out.println("Your search has no results");
+		List<WebElement> searchResults = driver.findElements(By
+				.xpath("//div[@class='s-item-container'][1]/div/div/div[@class='a-fixed-left-grid-col a-col-right']"));
+		System.out.println(searchResults.size());
+		try {
+			if (searchResults.size() > 0) {
+				String title, author = null;
+				int i = 0;
+				for (WebElement el : searchResults) {
+					((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", el);
+					i++;
+					title = el.findElement(By.cssSelector("h2.a-size-medium.s-inline.s-access-title.a-text-normal"))
+							.getText();
+					System.out.println("Title:" + i + "-" + title);
+					if(el.findElements(By.cssSelector("div.a-row.a-spacing-small > div:nth-child(2)")).size()>0){
+						author = el.findElement(By.cssSelector("div.a-row.a-spacing-small > div:nth-child(2)")).getText();
+					}
+					System.out.println("Author:" + i + "-" + (author.isEmpty()?"Author Not Found":author));
+				}
+			} else
+				System.out.println("Your search has no results");
+		} catch (Exception e) {
+			System.out.println(e.getLocalizedMessage());
+		}
 	}
 }
